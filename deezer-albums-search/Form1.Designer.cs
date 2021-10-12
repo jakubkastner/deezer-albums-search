2bdf7471523c43f76120f6eaa61046b6a8e4622e
Jakub Kastner <jakvb.kastner@gmail.com>
2021-10-12 18:33:33 +0200

    new icon

--- deezer-albums-search/Form1.Designer.cs
+++ deezer-albums-search/Form1.Designer.cs
@@ -112,8 +112,8 @@
             this.treeListView1.SelectedForeColor = System.Drawing.Color.White;
             this.treeListView1.ShowGroups = false;
             this.treeListView1.ShowImagesOnSubItems = true;
-            this.treeListView1.Size = new System.Drawing.Size(848, 436);
-            this.treeListView1.TabIndex = 7;
+            this.treeListView1.Size = new System.Drawing.Size(850, 436);
+            this.treeListView1.TabIndex = 0;
             this.treeListView1.UseCompatibleStateImageBehavior = false;
             this.treeListView1.UseFiltering = true;
             this.treeListView1.View = System.Windows.Forms.View.Details;
@@ -163,7 +163,7 @@
             this.button3.Location = new System.Drawing.Point(3, 125);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(125, 23);
-            this.button3.TabIndex = 2;
+            this.button3.TabIndex = 3;
             this.button3.Text = "search";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -182,7 +182,7 @@
             this.label1.Location = new System.Drawing.Point(3, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 19);
-            this.label1.TabIndex = 14;
+            this.label1.TabIndex = 8;
             this.label1.Text = "artist:";
             // 
             // label2
@@ -192,7 +192,7 @@
             this.label2.Location = new System.Drawing.Point(3, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(129, 19);
-            this.label2.TabIndex = 14;
+            this.label2.TabIndex = 9;
             this.label2.Text = "album:";
             // 
             // statusStrip1
@@ -203,7 +203,7 @@
             this.statusStrip1.Location = new System.Drawing.Point(0, 439);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(851, 22);
-            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
@@ -249,7 +249,7 @@
             this.splitContainer1.Size = new System.Drawing.Size(984, 461);
             this.splitContainer1.SplitterDistance = 131;
             this.splitContainer1.SplitterWidth = 2;
-            this.splitContainer1.TabIndex = 15;
+            this.splitContainer1.TabIndex = 0;
             // 
             // checkBox1
             // 
@@ -257,7 +257,7 @@
             this.checkBox1.Location = new System.Drawing.Point(3, 102);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(113, 17);
-            this.checkBox1.TabIndex = 16;
+            this.checkBox1.TabIndex = 2;
             this.checkBox1.Text = "search all releases";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
@@ -268,7 +268,7 @@
             this.label3.Location = new System.Drawing.Point(3, 355);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(125, 19);
-            this.label3.TabIndex = 15;
+            this.label3.TabIndex = 7;
             this.label3.Text = "the path is not selected";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
@@ -284,7 +284,7 @@
             this.button2.Location = new System.Drawing.Point(3, 377);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 23);
-            this.button2.TabIndex = 2;
+            this.button2.TabIndex = 4;
             this.button2.Text = "change path";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
@@ -301,7 +301,7 @@
             this.button4.Location = new System.Drawing.Point(3, 406);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(125, 23);
-            this.button4.TabIndex = 2;
+            this.button4.TabIndex = 5;
             this.button4.Text = "download cover";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.Button4_Click);
@@ -318,7 +318,7 @@
             this.button1.Location = new System.Drawing.Point(3, 435);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 23);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 6;
             this.button1.Text = "download tracklist";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
@@ -334,7 +334,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Deezer search";
+            this.Text = "Deezer albums search";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
